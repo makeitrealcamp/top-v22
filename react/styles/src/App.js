5d19d82bd846acb9df8b55b40396318c7cee6c19
Sergio <sergio@bellpi.com>
2022-05-10 17:08:04 -0500

    actualizando rama hasta el 10 de mayo

--- react/styles/src/App.js
+++ react/styles/src/App.js
@@ -3,7 +3,7 @@ import Button from "./components/Button";
 function App() {
   return (
     <div className="App">
-      <h1>Hola Make It Real</h1>
+      <h1>Hola Make It Real </h1>
       <Button />
     </div>
   );
